316e13be3262d9d1d3a8d4fe001cb9645371fa98
Luis Andre de Lima <luislimamn@gmail.com>
2019-06-05 09:28:43 -0300

    Initial Commit

---
+++ FrasesDoDia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FrasesDoDia
+//
+//  Created by Luis Lima on 05/06/2019.
+//  Copyright © 2019 Andr3ziim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
